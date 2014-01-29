52172a4e58a6dcd3285d8d0c3a40606ef41c0635
uakaminoe <noe.villegas333@gmail.com>
2014-01-29 07:19:56 -0600

    Solo unos cambios en las flechas del Slider

--- js/default.js
+++ js/default.js
@@ -1,14 +1,21 @@
 //alert("Si entra");
 $(document).ready(function(){
     var heightSlider =$('#slider').css('height').replace("px","");
-    var arrowPosition=(heightSlider/2)+10;
-    $("#slider").append(iconBuild('arrow-circle-o-left','3','arrow'));
-    $("#slider").append(iconBuild('arrow-circle-o-right','3','arrow right'));
+    var colorArrow=($('#slider').attr('color-arrow'))?$('#slider').attr('color-arrow'):"#333";
+    console.log(colorArrow)
+    var arrowPosition=(heightSlider/2)-14;
+    $("#slider").append(iconBuild('arrow-circle-o-left','3','arrow left','top:'+arrowPosition+'px;color:'+colorArrow));
+    $("#slider").append(iconBuild('arrow-circle-o-right','3','arrow right','top:'+arrowPosition+'px;color:'+colorArrow));
     
 });
 
-function iconBuild(name,size,extraclass){
+function iconBuild(name,size,extraclass,style){
     var extraClass=(extraclass)?extraclass:'';
+    var Styel=(style)?style:"";
     var iconHtml='<i class=" fa fa-'+size+'x fa-' ;
-    return iconHtml+= name+" "+extraClass+'"></i>';
+    return iconHtml+= name+" "+extraClass+'" style="'+style+'"></i>';
+}
+
+function getSliderNo(){
+    $('#slider')
 }
